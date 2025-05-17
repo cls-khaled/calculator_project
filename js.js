e72808d3fc5662e5846c23f9c1394f210be10598
Khaled Fathi <dev@khaledfathi.com>
2025-05-17 19:10:44 +0300

    calc,button ok, check point

--- js.js
+++ js.js
@@ -1,58 +1,144 @@
-// UI Elements 
+/* ##### Global Constants #####*/
+const
+    ADD = '+',
+    SUBTRACT = '-',
+    MULTIPLY = 'x',
+    DIVIDE = '÷',
+    EQUAL = '=',
+    //
+    MAX_MONITOR_LENGTH = 13;
+
+/* ##### Variables#####*/
 const
     //result monitor
     monitorValue = document.getElementById('monitor-value'),
     monitorOperator = document.getElementById('monitor-operator'),
-    // operators buttons 
-    dividedOperator = document.getElementById('btn-divide'),
-    multiplyOperator = document.getElementById('btn-multiply'),
-    subtractOperator = document.getElementById('btn-subtract'),
-    equalOperator = document.getElementById('btn-equal'),
-    addOperator = document.getElementById('btn-add'),
     //clear button
     allClear = document.getElementById('ac-btn'),
-    clearEntry = document.getElementById('ce-btn'),
-    //number buttons
-    button0 = document.getElementById('btn-0'),
-    button1 = document.getElementById('btn-1'),
-    button2 = document.getElementById('btn-2'),
-    button3 = document.getElementById('btn-3'),
-    button4 = document.getElementById('btn-4'),
-    button5 = document.getElementById('btn-5'),
-    button6 = document.getElementById('btn-6'),
-    button7 = document.getElementById('btn-7'),
-    button8 = document.getElementById('btn-8'),
-    button9 = document.getElementById('btn-9'),
-    //dot button 
-    buttonDot = document.getElementById('btn-dot');
+    clearEntry = document.getElementById('ce-btn');
+let
+    // calculation values
+    isRealNumber = false;
+operator = null,
+    lastNumber = null,
+    currentNumber = 0,
+    numberAsString = "";
+
+/* ##### Core #####*/
+function calculate(operator, lastValue, currentValue) {
+    switch (operator) {
+        case ADD:
+            return lastValue + currentValue
+        case SUBTRACT:
+            return lastValue - currentValue
+        case MULTIPLY:
+            return lastValue * currentValue
+        case DIVIDE:
+            return lastValue / currentValue
+    }
+}
+function isLimitExceeded(value) {
+    return value.length > MAX_MONITOR_LENGTH;
+}
+/* ##### Events #####*/
+//-------- operations buttons event
+let operationButtons = [
+    document.getElementById('btn-divide'),
+    document.getElementById('btn-multiply'),
+    document.getElementById('btn-subtract'),
+    document.getElementById('btn-equal'),
+    document.getElementById('btn-add'),
+]
+for (let operationButton of operationButtons) {
+    operationButton.addEventListener('click', () => handleOperationButtonClick(operationButton));
+}
+//-------- digit buttons event
+let digitButtons = [
+    document.getElementById('btn-0'),
+    document.getElementById('btn-1'),
+    document.getElementById('btn-2'),
+    document.getElementById('btn-3'),
+    document.getElementById('btn-4'),
+    document.getElementById('btn-5'),
+    document.getElementById('btn-6'),
+    document.getElementById('btn-7'),
+    document.getElementById('btn-8'),
+    document.getElementById('btn-9'),
+    document.getElementById('btn-dot')
+]
+for (let digitButton of digitButtons) {
+    digitButton.addEventListener('click', () => handleDigitButtonClick(digitButton));
+}
+clearEntry.addEventListener('click', handleClearEntryClick);
+allClear.addEventListener('click', handleAllClearClick)
+//--------
+
+/* ##### Event Handler #####*/
+function handleOperationButtonClick(operationButton) {
+    operator = operationButton.innerText != EQUAL ? operationButton.innerText : operator ;
+    monitorOperator.innerText = operator != EQUAL ? operator : "";
+    //
+    if (lastNumber && operator) {
+        let calcResult = calculate(operator, lastNumber, currentNumber);
+        lastNumber = calcResult;
+        monitorValue.innerText = lastNumber;
+    } else {
+        lastNumber = currentNumber;
+    }
+    //for next operation 
+    if(operationButton.innerText == EQUAL) monitorOperator.innerText = ""; 
+    numberAsString = "";
+    isRealNumber=false;
+    //
+    displayCalculatorState();
+}
+
+function handleDigitButtonClick(digitButton) {
+    let digit = digitButton.innerText;
+    //prevent multiple dots in number 
+    if (digit == '.') {
+        if (!isRealNumber) {
+            numberAsString += digit;
+            isRealNumber = true;
+        }
+    } else {
+        numberAsString += digit;
+    }
+
+    if (!isLimitExceeded(numberAsString)) {
+        currentNumber = parseFloat(numberAsString);
+        monitorValue.innerText = numberAsString;
+    }
+    displayCalculatorState();
+}
+
+function handleClearEntryClick() {
+    currentNumber = 0;
+    numberAsString = "";
+    monitorValue.innerText = "";
+}
+
+function handleAllClearClick() {
+    monitorValue.innerText = "";
+    monitorOperator.innerHTML = "";
+    currentNumber = 0;
+    lastNumber = null;
+    operator = null;
+    numberAsString = "";
+}
+
+/* ##### Testing #####*/
+function displayCalculatorState() {
+    console.log(
+        '\n| operator : ', operator,
+        '\n| last number : ', lastNumber,
+        '\n| current number : ', currentNumber,
+        '\n| monitor value : ', monitorValue.innerText,
+        '\n| monitor operator : ', monitorOperator.innerText,
+        '\n| result : ', calculate(operator, lastNumber, currentNumber)
+
+    );
+}
 
 
-/* ################################ */
-/* ##### WRITE YOUR CODE HERE #####*/
-/* ################################ */
 
-
-
-// element existance test; 
-console.debug(
-`Elements Variable Test: 
-variable 'monitor value' ${monitorValue ? '[Pass]' : '[Fail]'}
-variable 'monitor operator' ${monitorOperator ? '[Pass]' : '[Fail]'}
-variable 'dividedOperator' ${dividedOperator ? '[Pass]' : '[Fail]'} 
-variable 'multiplyOperator' ${multiplyOperator ? '[Pass]' : '[Fail]'} 
-variable 'subtractOperator' ${subtractOperator ? '[Pass]' : '[Fail]'} 
-variable 'equalOperator' ${equalOperator ? '[Pass]' : '[Fail]'} 
-variable 'addOperator' ${addOperator ? '[Pass]' : '[Fail]'} 
-variable 'allClear' ${allClear ? '[Pass]' : '[Fail]'} 
-variable 'clearEntry' ${clearEntry ? '[Pass]' : '[Fail]'} 
-variable 'button1' ${button1 ? '[Pass]' : '[Fail]'} 
-variable 'button2' ${button2 ? '[Pass]' : '[Fail]'} 
-variable 'button3' ${button3 ? '[Pass]' : '[Fail]'} 
-variable 'button4' ${button4 ? '[Pass]' : '[Fail]'} 
-variable 'button5' ${button5 ? '[Pass]' : '[Fail]'} 
-variable 'button6' ${button6 ? '[Pass]' : '[Fail]'} 
-variable 'button7' ${button7 ? '[Pass]' : '[Fail]'} 
-variable 'button8' ${button8 ? '[Pass]' : '[Fail]'} 
-variable 'button9' ${button9 ? '[Pass]' : '[Fail]'} 
-variable 'buttonDot' ${buttonDot ? '[Pass]' : '[Fail]'} 
-`);
